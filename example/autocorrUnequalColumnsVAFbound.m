9527207818ce94c7a8d5fb62b9a057c5ab30c33c
Unknown <itu.pablo@gmail.com>
2018-04-11 22:50:49 -0400

    Improved unequal autocorr example
    Something is not right: if I standardize the column energies, the empirical spectral density is somewhat far from the *true*, and the *true* bound doesnt appear to hold even on expectation (empirical bound does hold)

--- example/autocorrUnequalColumnsVAFbound.m
+++ example/autocorrUnequalColumnsVAFbound.m
@@ -1,22 +1,25 @@
 %% Example of Fourier-based bound for autocorrelated data
-
+close all
 %% Construct: a data matrix with independent but autocorrelated columns
 N=1e4;
 M=30;
-dc=.1;
-X=(randn(N,M)+dc);
+X=(randn(N,M)); %M realizations of N-sample white noise
 for j=1:M
-sigma=10*(1+5*abs(randn));
-Ga=exp(-[0:N-1]'/sigma); %Frequency-space shaping of signal
-Ga(2:end)=Ga(2:end)+conj(flipud(Ga(2:end))); %Ensuring a real-valued filter
+sigma=50*(1+5*abs(randn));
+Ga=exp(-[0:N-1]'/sigma)+.1; %Frequency-space shaping of signal
+%Ga=ones(N,1);% White noise
+Ga(20:20:N)=Ga(20:20:N)+50./sqrt([1:20:N]'); %1/f signal
+Ga((N/2)+2:N)=conj(flipud(Ga(2:(N/2)))); %Ensuring a real-valued filter
+%Ga(N/2+1)=0;
+Ga(1)=abs(3*randn); %Adding more DC
 G(:,j)=Ga;
 
-X(:,j)=ifft(fft(X(:,j),[],1).*G(:,j)); %Filtering
-g=ifft(G(:,j));
-g=g(1:200); %200-th order FIR approximation;
+X(:,j)=ifft(fft(X(:,j),[],1).*Ga); %Filtering
+%g=ifft(G(:,j));
+%g=g(1:200); %200-th order FIR approximation;
 end
-X=X./sqrt(sum(X.^2,1)); %Normalizing energy per column: this is not needed, but if not done the bounds are trivial for the filters used (the dominant difference between columns is the energy, not their autocorrelations)
-
+X=sqrt(N)*X./sqrt(sum(X.^2,1)); %Normalizing energy per column: this is not needed, but if not done the bounds are trivial for the filters used (the dominant difference between columns is the energy, not their autocorrelations)
+%G=G./sqrt(2*N*sum(X.^2,1));
 %% Plots
 figure('Units','Normalized','OuterPosition',[0 0 1 1])
 subplot(2,2,1) %Data
@@ -27,7 +30,7 @@ subplot(2,2,2) %Data autocorrelation
 hold on
 r=0;
 for j=1:M
-rj=autocorr(X(:,j),'NumLags',249);
+rj=autocorr(X(:,j),'NumLags',999);
 plot(rj)
 r=r+rj*sum(X(:,j).^2);
 end
@@ -40,9 +43,8 @@ subplot(2,2,3) %Bounds
 l=VAF(X'*X); %Computing X'*X because it is cheaper
 plot(l,'LineWidth',2,'DisplayName','Empirical eigenvalues')
 hold on
-G1=G;
-G1(1,:)=G1(1,:)*(dc*sqrt(N)); %G is the filter spectrum. But the original signal spectrum was flat except for the DC component. This is the theoretical DC component after filtering.
-b=VAF(sum(abs(G1).^2,2)); %Fourier-based bound
+G1=abs(G).^2;
+b=VAF(sum(G1,2)); %Fourier-based bound
 plot(b,'LineWidth',2,'DisplayName','*True* column spectrum based bound')
 F=fft(X,[],1);
 c=VAF(sum(abs(F).^2,2));
@@ -52,9 +54,17 @@ plot(d,'LineWidth',2,'DisplayName','Empirical column energy based bound')
 C=dct(X,[],1);
 e=VAF(sum(C.^2,2));
 plot(e,'LineWidth',2,'DisplayName','Empirical column DCT based bound')
-plot(d+(1-d).*c(1:length(d))','LineWidth',2,'DisplayName','Experimental')
+%plot(d+(1-d).*c(1:length(d))','LineWidth',2,'DisplayName','Experimental') %This works as bound IF all columns have same energy (ie., d is a straight line)
+plot([1:M]/M+(1-[1:M]/M).*c(1:M)','LineWidth',2,'DisplayName','Experimental 2') 
 axis([1 M 0 1])
 title('Eigenvalues')
 legend('Location','SouthEast')
 ylabel('Variance Accounted For (%)')
 xlabel('Dimensions')
+
+subplot(2,2,4)
+plot(sum(G1,2),'LineWidth',2,'DisplayName','True spectral density')
+hold on
+plot(sum(abs(F).^2,2)/N,'LineWidth',2,'DisplayName','Empirical spectral density')
+set(gca,'YScale','log')
+legend
